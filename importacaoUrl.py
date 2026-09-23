0a46ae176ddefeb4fffedfa161cb9b613ba8a857
jhenrique131 <jhenrique131fc@gmail.com>
2026-09-23 15:09:40 -0300

    Exercico de importação de dados a partir de uma URL - Importação dos dados

---
+++ importacaoUrl.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+
+colnames = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'Class']
+
+iris = pd.read_csv(url, names=colnames)
+
+print(iris.head())
+print(iris.shape)
