e8018741fc2b7fd3cd2119785adaaed1613fde74
jhenrique131 <jhenrique131fc@gmail.com>
2026-09-23 14:54:39 -0300

    Importação dos dados de origem - Arquivo csv

---
+++ importacaoDados.py
@@ -0,0 +1,13 @@
+#Projeto Análise de dados da COVID19 no estado de São Paulo
+#Este projeto analisa os dados dos casos de covid 19 no
+#estado de São Paulo no período de fevereiro de 2020 a setembro de 2021.
+import numpy as np
+import pandas as pd
+
+arquivo_csv = 'E:\0 - Portifolio\python\TratamentoExploracaoAnalise_Projeto1\dados\dados_covid_sp.csv'
+covid_sp = pd.read_csv(arquivo_csv, sep=';', encoding='utf-8')
+
+covid_sp.head()
+
+
+
