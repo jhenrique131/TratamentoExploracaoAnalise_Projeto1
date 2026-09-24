3356620c70ff49563f96d22ca18a21472ea59915
jhenrique131 <jhenrique131fc@gmail.com>
2026-09-24 10:49:48 -0300

    Instalação do scikit-learn e importação das bibliotecas

--- datasetsDoPython.py
+++ datasetsDoPython.py
@@ -1,5 +1,10 @@
 import statsmodels.api as sm
+import sklearn
+from sklearn import datasets
 
 cancer = sm.datasets.cancer.load_pandas().data
 
 print(cancer.head())
+print(cancer.shape)
+print(type(cancer))
+
