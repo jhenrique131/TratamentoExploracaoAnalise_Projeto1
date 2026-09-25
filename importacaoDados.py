5d177fd2617e7441de933676670894379a2d277b
jhenrique131 <jhenrique131fc@gmail.com>
2026-09-25 10:00:01 -0300

    Os prints da função carrega_dados_covid_sp foram comentados.

--- importacaoDados.py
+++ importacaoDados.py
@@ -8,8 +8,8 @@ def carrega_dados_covid_sp():
     arquivo_csv = "E:/0 - Portifolio/python/TratamentoExploracaoAnalise_Projeto1/dados/dados_covid_sp.csv"
     covid_sp = pd.read_csv(arquivo_csv, sep=';', encoding='utf-8')
 
-    print(covid_sp.head())
-    print(covid_sp.shape)
+    #print(covid_sp.head())
+    #print(covid_sp.shape)
 
     return covid_sp 
 
