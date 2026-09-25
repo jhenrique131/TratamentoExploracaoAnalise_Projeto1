9a80fdb95c7102e317dd955abf37d8e3e807368b
jhenrique131 <jhenrique131fc@gmail.com>
2026-09-25 10:09:12 -0300

    Alterando o nome das colunas nome_munic e datahora

--- renomeandoColunas.py
+++ renomeandoColunas.py
@@ -1,7 +1,18 @@
+#Importa a função do arquivo importaçãoDados.py
 from importacaoDados import carrega_dados_covid_sp
 
-df = carrega_dados_covid_sp()
+#Chama a função carrega_dados_covid_sp()
+covid_sp = carrega_dados_covid_sp()
 
 print("Visualizando as primeira linhas do DataFrame")
 
-print(df.head())
+#Visualização dos registros
+#print(covid_sp.head())
+
+#Alterando o nome da coluna e sobreescrevendo a tabela
+covid_sp.rename(columns={'nome_munic':'municipio'}, inplace=True)#Inplace=True faz a troca permanente
+
+#Alterando a coluna de datahora para data
+covid_sp.rename(columns={'datahora':'data'}, inplace=True)
+
+print(covid_sp.head())
